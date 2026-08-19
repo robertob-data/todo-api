3ac1e519b9ad592ba8dddea58dd9ca2559e4d541
Roberto Dias <batistape083@gmail.com>
2026-08-18 21:37:12 -0300

    docs: adiciona docstrings nas rotas do todo-api

--- main.py
+++ main.py
@@ -18,11 +18,31 @@ def ler_raiz():
 
 @app.get('/tarefas')
 def listar_tarefas():
+    """
+Lista todas as tarefas registradas.
+
+Args:
+    sem args.
+
+Returns:
+    retorna a lista das tarefas.
+    """
+    
     return tarefaslista
 
 @app.post('/tarefas', status_code=201)
 def criar_tarefas(tarefa: Tarefa):
+    """
+Cria uma nova tarefas.
+
+Args:
+    tarefa (Tarefa): a classe usada para criar uma nova tarefa, o 'placeholder'
+
+Returns:
+    retorna uma mensagem de comfirmaçao + a tarefa em si.
+    """
     
+    #Global temporario, unicamente para testes, sera corrigido para sistema de classes/POO
     global proximo_id
     
     tarefa.id = proximo_id
@@ -34,6 +54,15 @@ def criar_tarefas(tarefa: Tarefa):
 
 @app.delete("/tarefas/{id}")
 def deletar_tarefas(id: int):
+    """
+deleta tarefas ativas.
+
+Args:
+    id (int): numero de identificaçao (ID) da tarefa a ser deletada.
+
+Returns:
+    retorna uma mensagem de sucesso ou erro.
+    """
     for tarefa in tarefaslista:
         if tarefa.id == id:
             tarefaslista.remove(tarefa)
@@ -43,6 +72,16 @@ def deletar_tarefas(id: int):
 
 @app.put("/tarefas/{tarefa_id}")
 def atualizar_tarefa(tarefa_id: int, dados: Tarefa):
+    """
+Atualiza as informaçoes de uma tarefa.
+
+Args:
+    tarefa_id (int): Identifica a tarefa
+    dados (Tarefa): quais novas informaçoes a tarefa recebera
+
+Returns:
+    retorna uma mensagen de sucesso ou erro.
+    """
     for i, tarefa in enumerate(tarefaslista):
         if tarefa.id == tarefa_id:
             dados.id = tarefa_id
